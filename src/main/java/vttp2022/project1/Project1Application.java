9a2941d5bb2a95993a9c4e827c397af023eef2e9
angieyumin1997 <angieyumin1997@yahoo.com.sg>
2022-05-04 17:16:02 +0800

    deploy to heroku

---
+++ src/main/java/vttp2022/project1/Project1Application.java
@@ -0,0 +1,13 @@
+package vttp2022.project1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Project1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Project1Application.class, args);
+	}
+
+}
---
+++ src/test/java/vttp2022/project1/Project1ApplicationTests.java
@@ -0,0 +1,13 @@
+package vttp2022.project1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Project1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
